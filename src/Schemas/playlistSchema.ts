3a8748b5fffb85e419582f80a52f2b943ebc3412
Samuelmm15 <alu0101359526@ull.edu.es>
2022-05-17 18:52:31 +0100

    Fixed errors

--- src/Schemas/playlistSchema.ts
+++ src/Schemas/playlistSchema.ts
@@ -1,7 +1,61 @@
 /* eslint-disable prefer-const */
 import {Playlists} from "../Interfaces/playlistInterface";
+import {Song} from "../Interfaces/songInterface";
 import {Schema} from 'mongoose';
 let validator = require('validator');
+
+const songPlaylistSchema = new Schema<Song>({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  author: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  duration: {
+    type: String,
+    required: true,
+    trim: true,
+    validate: (value: string) => {
+      let pattern = /([0-5]?[0-9]):(?:[012345]\d)$/g;
+      let result = value.match(pattern)?.toString();
+      if (result === undefined) {
+        throw new Error('Time format not valid, try again with this format -> MM:SS');
+      }
+    },
+  },
+  genre: {
+    type: [String],
+    required: true,
+    enum: ['Rock', 'Pop', 'Rap', 'Jazz', 'Country', 'Electronic',
+      'Folk', 'Hip-Hop', 'Classic', 'Merengue', 'Metal', 'Electro',
+      'Reggaeton', 'Salsa', 'Samba', 'Tango', 'Techno', 'Other'],
+    validate: (value: string[]) => {
+      value.forEach((genre) => {
+        if (!validator.isAlpha(genre)) {
+          throw new Error('Note title must contain alphabet characters');
+        }
+      });
+    },
+  },
+  single: {
+    type: Boolean,
+    required: true,
+  },
+  reproductionNumber: {
+    type: String,
+    required: true,
+    trim: true,
+    validate: (value: string) => {
+      if (!validator.isDecimal(value)) {
+        throw new Error('Reproduction Number must be a number');
+      }
+    },
+  },
+});
 
 export const PlaylistSchema = new Schema<Playlists>({
   name: {
@@ -11,58 +65,7 @@ export const PlaylistSchema = new Schema<Playlists>({
     trim: true,
   },
   songs: {
-    type: [{
-      name: {
-        type: String,
-        required: true,
-        trim: true,
-      },
-      author: {
-        type: String,
-        required: true,
-        trim: true,
-      },
-      duration: {
-        type: String,
-        required: true,
-        trim: true,
-        validate: (value: string) => {
-          let pattern = /([0-5]?[0-9]):(?:[012345]\d)$/g;
-          let result = value.match(pattern)?.toString();
-          if (result === undefined) {
-            throw new Error('Time format not valid, try again with this format -> MM:SS');
-          }
-        },
-      },
-      genre: {
-        type: [String],
-        required: true,
-        enum: ['Rock', 'Pop', 'Rap', 'Jazz', 'Country', 'Electronic',
-          'Folk', 'Hip-Hop', 'Classic', 'Merengue', 'Metal', 'Electro',
-          'Reggaeton', 'Salsa', 'Samba', 'Tango', 'Techno', 'Other'],
-        validate: (value: string[]) => {
-          value.forEach((genre) => {
-            if (!validator.isAlpha(genre)) {
-              throw new Error('Note title must contain alphabet characters');
-            }
-          });
-        },
-      },
-      single: {
-        type: Boolean,
-        required: true,
-      },
-      reproductionNumber: {
-        type: String,
-        required: true,
-        trim: true,
-        validate: (value: string) => {
-          if (!validator.isDecimal(value)) {
-            throw new Error('Reproduction Number must be a number');
-          }
-        },
-      },
-    }],
+    type: [songPlaylistSchema],
     required: true,
   },
   duration: {
