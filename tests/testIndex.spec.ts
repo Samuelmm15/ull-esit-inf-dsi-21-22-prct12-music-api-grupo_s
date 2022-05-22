47548b7f328fcb6074dfb52c9b6771cd0777f8db
Samuel Martín Morales <72341631+Samuelmm15@users.noreply.github.com>
2022-05-22 09:20:18 +0100

    Moved thunder client collection to tests directory

--- tests/testIndex.spec.ts
+++
@@ -1,9 +0,0 @@
-import 'mocha';
-import {expect} from 'chai';
-import {add} from '../src/index';
-
-describe('First Test', () => {
-  it('First test', () => {
-    expect(add(1, 1)).to.be.equal(2);
-  });
-});
